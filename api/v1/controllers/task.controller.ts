bf88fc922be6bacdef44f172d69b5faf5da494f6
NTPhus <phu444080@gmail.com>
2025-03-16 21:29:54 +0700

    Tạo công việc

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -152,3 +152,21 @@ export const changeMulti = async (req: Request, res: Response) => {
         });
     }
 }
+
+export const create = async (req: Request, res: Response) => {
+      try {
+        const task = new Task(req.body);
+        const data = await task.save();
+    
+        res.json({
+          code: 200,
+          data: data,
+          message: "Tạo thành công",
+        });
+      } catch (error) {
+        res.json({
+          code: 400,
+          message: "Lỗi",
+        });
+      }
+}
